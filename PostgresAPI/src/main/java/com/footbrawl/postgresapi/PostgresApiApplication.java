e8fe3aac6f5a9d6a15c9b88c2cf38acd08ac6d41
fede <fedebiella2002@gmail.com>
2024-03-06 15:37:48 +0100

    Created new PostgreSQL server and modified MainServer name

---
+++ PostgresAPI/src/main/java/com/footbrawl/postgresapi/PostgresApiApplication.java
@@ -0,0 +1,13 @@
+package com.footbrawl.postgresapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PostgresApiApplication.class, args);
+  }
+
+}
