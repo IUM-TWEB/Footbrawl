1c7c63efd3a2cd58c83faec88c73dc604de51d6c
umberto <umberto.rastello@edu.unito.it>
2024-03-21 15:04:36 +0100

    add frontend directory with react-vite end the first components. Delete public directory in the MainServer project

---
+++ frontend/src/ImmaginiCarousel.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+//import { Carousel } from 'react-bootstrap'; // Assumendo che stai usando react-bootstrap
+
+const ImmaginiCarousel = () => { // Rinominato per evitare conflitti
+
+  const immagini = [{
+    titolo: "niggio1",
+    src: "../public/milan.jpg",
+    alt : "First slide"
+  }, {
+    titolo: "niggio2",
+    src: "../public/sciarpe-galatasaray.jpg",
+    alt : "Second slide",
+  }, {
+    titolo: "niggio3",
+    src: "../public/foto-bayern.jpeg",
+    alt: "Third slide",
+  }];
+
+  return (
+    <Carousel>
+      {immagini.map((img) => (
+        <Carousel.Item key={img.titolo}> {/* Aggiunto key per migliorare le performance */}
+          <img
+            className="d-block w-100"
+            src={img.src}
+            alt={img.alt}
+          />
+          <Carousel.Caption>
+            <h3>{img.titolo}</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default ImmaginiCarousel;
