7205ea0225eda4ab01f7c560121d30df4c8a7a5b
fRichs07 <riccardotricarico07@gmail.com>
2024-05-15 18:11:00 +0200

    set up for the upload of the information of the ranking of each club in postgres

--- MongoAPI/queries/games_queries.js
+++ MongoAPI/queries/games_queries.js
@@ -12,4 +12,48 @@ const getByClub = (comp, season, club_id) => {
     return games.find({competition_id:comp, season:season, home_club_id :club_id }, {}, null)
 }
 
-module.exports = {getByClub, getByComp, getById}
+const getPosition = (teamId) => {
+    console.log(typeof teamId)
+    // teamId = 2700
+    return games.aggregate([
+        {
+            $match: {
+                $or: [
+                    { home_club_id: teamId },
+                    { away_club_id: teamId }
+                ]
+            }
+        },
+        {
+            $sort: { date: -1 }
+        },
+        {
+            $group: {
+                _id: {
+                    season: "$season",
+                    competition_id: "$competition_id"
+                },
+                last_match_date: { $first: "$date" },
+                club_id: { $first: { $cond: [{ $eq: ["$home_club_id", teamId] }, "$home_club_id", "$away_club_id"] } },
+                position: { $first: { $cond: [{ $eq: ["$home_club_id", teamId] }, "$home_club_position", "$away_club_position"] } }
+            }
+        },
+        {
+            $match: {
+                position: { $ne: null }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                season: "$_id.season",
+                competition_id: "$_id.competition_id",
+                last_match_date: 1,
+                club_id: 1,
+                position: 1
+            }
+        }
+    ])
+}
+
+module.exports = {getByClub, getByComp, getById, getPosition}
