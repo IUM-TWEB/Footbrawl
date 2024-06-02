8ecf82953c1baa5030421cd51f9ef5baa48fb29e
fede <fedebiella2002@gmail.com>
2024-06-02 16:02:19 +0200

    Modified the PaginaUtente page to create your favorite team

--- frontend/src/page/PaginaUser.jsx
+++ frontend/src/page/PaginaUser.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {useAuth} from '../context/AuthContext';
 import {useNavigate} from 'react-router-dom';
+import soccerFieldImage from '../img/soccer-green-field.png';
 
 const PaginaUser = () => {
   const {username, favoritePlayers, favoriteClubs, logout} = useAuth();
@@ -13,6 +14,13 @@ const PaginaUser = () => {
 
   const [playerNames, setPlayerNames] = useState([]);
   const [clubNames, setClubNames] = useState([]);
+  const [formation, setFormation] = useState('4-4-2');
+  const [selectedFormation, setSelectedFormation] = useState({
+    forwards: [],
+    midfielders: [],
+    defenders: [],
+    goalkeeper: null
+  });
 
   useEffect(() => {
     if (favoritePlayers.length > 0) {
@@ -85,6 +93,40 @@ const PaginaUser = () => {
     setSelectedClub(club);
   };
 
+  const handleFormationChange = (event) => {
+    setFormation(event.target.value);
+    // Reset della formazione selezionata quando si cambia la formazione
+    setSelectedFormation({
+      forwards: [],
+      midfielders: [],
+      defenders: [],
+      goalkeeper: null
+    });
+  };
+
+  const handlePlayerSelection = (player, position) => {
+    setSelectedFormation(prevFormation => {
+      const newFormation = {...prevFormation};
+      switch (position) {
+        case 'forward':
+          newFormation.forwards = newFormation.forwards.includes(player) ? newFormation.forwards.filter(p => p !== player) : [...newFormation.forwards, player];
+          break;
+        case 'midfielder':
+          newFormation.midfielders = newFormation.midfielders.includes(player) ? newFormation.midfielders.filter(p => p !== player) : [...newFormation.midfielders, player];
+          break;
+        case 'defender':
+          newFormation.defenders = newFormation.defenders.includes(player) ? newFormation.defenders.filter(p => p !== player) : [...newFormation.defenders, player];
+          break;
+        case 'goalkeeper':
+          newFormation.goalkeeper = newFormation.goalkeeper === player ? null : player;
+          break;
+        default:
+          break;
+      }
+      return newFormation;
+    });
+  };
+
   const formatValue = (value) => {
     return value === -1 ? 'non disponibile' : value + ' euro';
   };
@@ -94,7 +136,7 @@ const PaginaUser = () => {
       <h1>Utente: {username ? username : 'utente'}</h1>
       {username && (
         <>
-          <div className="my-4">
+          <div className="my-5">
             <h2>Giocatori Preferiti:</h2>
             {favoritePlayers.length > 0 ? (
               <div className="row align-items-center" style={{minHeight: '50vh'}}>
@@ -147,7 +189,9 @@ const PaginaUser = () => {
             )}
           </div>
 
-          <div className="my-4">
+          <hr className="my-custom-hr"/>
+
+          <div className="my-5">
             <h2>Club Preferiti:</h2>
             {favoriteClubs.length > 0 ? (
               <div className="row align-items-center" style={{minHeight: '50vh'}}>
@@ -182,6 +226,54 @@ const PaginaUser = () => {
             ) : (
               <p>Nessun club preferito</p>
             )}
+          </div>
+
+          <hr className="my-custom-hr"/>
+
+          <div className="my-5">
+            <div className="row">
+              <div className="col-md-6">
+                <h2 className="mb-4">Formazione della squadra preferita:</h2>
+                <select value={formation} onChange={handleFormationChange} className="form-select mb-3">
+                  <option value="4-4-2">4-4-2</option>
+                  <option value="4-3-3">4-3-3</option>
+                  <option value="3-5-2">3-5-2</option>
+                  <option value="3-4-3">3-4-3</option>
+                  <option value="5-3-2">5-3-2</option>
+                </select>
+
+                <h3>Giocatori selezionati:</h3>
+                <div className="text-center">
+                  <h4>Attaccanti</h4>
+                  <ul className="list-unstyled">
+                    {selectedFormation.forwards.map(player => (
+                      <li key={player.playerId}>{player.name}</li>
+                    ))}
+                  </ul>
+                  <h4>Centrocampisti</h4>
+                  <ul className="list-unstyled">
+                    {selectedFormation.midfielders.map(player => (
+                      <li key={player.playerId}>{player.name}</li>
+                    ))}
+                  </ul>
+                  <h4>Difensori</h4>
+                  <ul className="list-unstyled">
+                    {selectedFormation.defenders.map(player => (
+                      <li key={player.playerId}>{player.name}</li>
+                    ))}
+                  </ul>
+                  <h4>Portiere</h4>
+                  <ul className="list-unstyled">
+                    {selectedFormation.goalkeeper && (
+                      <li key={selectedFormation.goalkeeper.playerId}>{selectedFormation.goalkeeper.name}</li>
+                    )}
+                  </ul>
+                </div>
+              </div>
+              <div className="col-md-6 text-end">
+                <img src={soccerFieldImage} alt="Soccer Field" className="img-fluid" style={{maxWidth: '100%'}}/>
+              </div>
+            </div>
           </div>
 
           <div className="d-flex justify-content-end w-100 mt-3">
