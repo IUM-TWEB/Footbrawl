8fb43bc2273e090cbf5fa8e2e28c6987ffbe915e
fede <fedebiella2002@gmail.com>
2024-06-25 11:57:43 +0200

    Added comments with javadoc in the package player

--- PostgresAPI/src/main/java/com/footbrawl/postgresapi/player/Player.java
+++ PostgresAPI/src/main/java/com/footbrawl/postgresapi/player/Player.java
@@ -7,6 +7,9 @@ import jakarta.persistence.Transient;
 
 import java.time.LocalDate;
 
+/**
+ * Entity class representing a Player.
+ */
 @Entity
 @Table(name = "players")
 public class Player {
@@ -43,11 +46,40 @@ public class Player {
   @Transient
   private int highest_market_value;
 
-
-  public Player(){
+  /**
+   * Default constructor.
+   */
+  public Player() {
 
   }
 
+  /**
+   * Constructor with all fields.
+   *
+   * @param player_id                            the ID of the player
+   * @param first_name                           the first name of the player
+   * @param last_name                            the last name of the player
+   * @param name                                 the full name of the player
+   * @param last_season                          the last season the player participated in
+   * @param current_club_id                      the current club ID of the player
+   * @param player_code                          the player code
+   * @param country_of_birth                     the country of birth of the player
+   * @param city_of_birth                        the city of birth of the player
+   * @param country_of_citizenship               the country of citizenship of the player
+   * @param date_of_birth                        the date of birth of the player
+   * @param sub_position                         the sub position of the player
+   * @param position                             the position of the player
+   * @param foot                                 the preferred foot of the player
+   * @param height_in_cm                         the height of the player in centimeters
+   * @param market_value_in_eur                  the market value of the player in euros
+   * @param highest_market_value_in_eur          the highest market value of the player in euros
+   * @param contract_expiration_date             the contract expiration date of the player
+   * @param agent_name                           the agent name of the player
+   * @param image_url                            the image URL of the player
+   * @param url                                  the URL of the player
+   * @param current_club_domestic_competition_id the current club domestic competition ID
+   * @param current_club_name                    the current club name of the player
+   */
   public Player(Long player_id, String first_name, String last_name, String name, Integer last_season, Integer current_club_id, String player_code, String country_of_birth, String city_of_birth, String country_of_citizenship, LocalDate date_of_birth, String sub_position, String position, String foot, Integer height_in_cm, String market_value_in_eur, String highest_market_value_in_eur, LocalDate contract_expiration_date, String agent_name, String image_url, String url, String current_club_domestic_competition_id, String current_club_name) {
     this.player_id = player_id;
     this.first_name = first_name;
@@ -77,211 +109,471 @@ public class Player {
     this.highest_market_value = getHighest_market_value();
   }
 
+  /**
+   * Gets the player ID.
+   *
+   * @return the player ID
+   */
   public Long getPlayer_id() {
     return player_id;
   }
 
-  public void setPlayer_id(Long playerId) {
-    this.player_id = playerId;
+  /**
+   * Sets the player ID.
+   *
+   * @param player_id the player ID
+   */
+  public void setPlayer_id(Long player_id) {
+    this.player_id = player_id;
   }
 
+  /**
+   * Gets the first name of the player.
+   *
+   * @return the first name of the player
+   */
   public String getFirst_name() {
     return first_name;
   }
 
+  /**
+   * Sets the first name of the player.
+   *
+   * @param first_name the first name of the player
+   */
   public void setFirst_name(String first_name) {
     this.first_name = first_name;
   }
 
+  /**
+   * Gets the last name of the player.
+   *
+   * @return the last name of the player
+   */
   public String getLast_name() {
     return last_name;
   }
 
+  /**
+   * Sets the last name of the player.
+   *
+   * @param last_name the last name of the player
+   */
   public void setLast_name(String last_name) {
     this.last_name = last_name;
   }
 
+  /**
+   * Gets the full name of the player.
+   *
+   * @return the full name of the player
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Sets the full name of the player.
+   *
+   * @param name the full name of the player
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Gets the last season the player participated in.
+   *
+   * @return the last season the player participated in
+   */
   public Integer getLast_season() {
     return last_season;
   }
 
+  /**
+   * Sets the last season the player participated in.
+   *
+   * @param last_season the last season the player participated in
+   */
   public void setLast_season(Integer last_season) {
     this.last_season = last_season;
   }
 
+  /**
+   * Gets the current club ID of the player.
+   *
+   * @return the current club ID of the player
+   */
   public Integer getCurrent_club_id() {
     return current_club_id;
   }
 
+  /**
+   * Sets the current club ID of the player.
+   *
+   * @param current_club_id the current club ID of the player
+   */
   public void setCurrent_club_id(Integer current_club_id) {
     this.current_club_id = current_club_id;
   }
 
+  /**
+   * Gets the player code.
+   *
+   * @return the player code
+   */
   public String getPlayer_code() {
     return player_code;
   }
 
+  /**
+   * Sets the player code.
+   *
+   * @param player_code the player code
+   */
   public void setPlayer_code(String player_code) {
     this.player_code = player_code;
   }
 
+  /**
+   * Gets the country of birth of the player.
+   *
+   * @return the country of birth of the player
+   */
   public String getCountry_of_birth() {
     return country_of_birth;
   }
 
+  /**
+   * Sets the country of birth of the player.
+   *
+   * @param country_of_birth the country of birth of the player
+   */
   public void setCountry_of_birth(String country_of_birth) {
     this.country_of_birth = country_of_birth;
   }
 
+  /**
+   * Gets the city of birth of the player.
+   *
+   * @return the city of birth of the player
+   */
   public String getCity_of_birth() {
     return city_of_birth;
   }
 
+  /**
+   * Sets the city of birth of the player.
+   *
+   * @param city_of_birth the city of birth of the player
+   */
   public void setCity_of_birth(String city_of_birth) {
     this.city_of_birth = city_of_birth;
   }
 
+  /**
+   * Gets the country of citizenship of the player.
+   *
+   * @return the country of citizenship of the player
+   */
   public String getCountry_of_citizenship() {
     return country_of_citizenship;
   }
 
+  /**
+   * Sets the country of citizenship of the player.
+   *
+   * @param country_of_citizenship the country of citizenship of the player
+   */
   public void setCountry_of_citizenship(String country_of_citizenship) {
     this.country_of_citizenship = country_of_citizenship;
   }
 
+  /**
+   * Gets the date of birth of the player.
+   *
+   * @return the date of birth of the player
+   */
   public LocalDate getDate_of_birth() {
     return date_of_birth;
   }
 
+  /**
+   * Sets the date of birth of the player.
+   *
+   * @param date_of_birth the date of birth of the player
+   */
   public void setDate_of_birth(LocalDate date_of_birth) {
     this.date_of_birth = date_of_birth;
   }
 
+  /**
+   * Gets the sub position of the player.
+   *
+   * @return the sub position of the player
+   */
   public String getSub_position() {
     return sub_position;
   }
 
+  /**
+   * Sets the sub position of the player.
+   *
+   * @param sub_position the sub position of the player
+   */
   public void setSub_position(String sub_position) {
     this.sub_position = sub_position;
   }
 
+  /**
+   * Gets the position of the player.
+   *
+   * @return the position of the player
+   */
   public String getPosition() {
     return position;
   }
 
+  /**
+   * Sets the position of the player.
+   *
+   * @param position the position of the player
+   */
   public void setPosition(String position) {
     this.position = position;
   }
 
+  /**
+   * Gets the preferred foot of the player.
+   *
+   * @return the preferred foot of the player
+   */
   public String getFoot() {
     return foot;
   }
 
+  /**
+   * Sets the preferred foot of the player.
+   *
+   * @param foot the preferred foot of the player
+   */
   public void setFoot(String foot) {
     this.foot = foot;
   }
 
+  /**
+   * Gets the height of the player in centimeters.
+   *
+   * @return the height of the player in centimeters
+   */
   public Integer getHeight_in_cm() {
     return height_in_cm;
   }
 
+  /**
+   * Sets the height of the player in centimeters.
+   *
+   * @param height_in_cm the height of the player in centimeters
+   */
   public void setHeight_in_cm(Integer height_in_cm) {
     this.height_in_cm = height_in_cm;
   }
 
+  /**
+   * Gets the market value of the player in euros.
+   *
+   * @return the market value of the player in euros
+   */
   public String getMarket_value_in_eur() {
     return market_value_in_eur;
   }
 
+  /**
+   * Sets the market value of the player in euros.
+   *
+   * @param market_value_in_eur the market value of the player in euros
+   */
   public void setMarket_value_in_eur(String market_value_in_eur) {
     this.market_value_in_eur = market_value_in_eur;
   }
 
+  /**
+   * Gets the highest market value of the player in euros.
+   *
+   * @return the highest market value of the player in euros
+   */
   public String getHighest_market_value_in_eur() {
     return highest_market_value_in_eur;
   }
 
+  /**
+   * Sets the highest market value of the player in euros.
+   *
+   * @param highest_market_value_in_eur the highest market value of the player in euros
+   */
   public void setHighest_market_value_in_eur(String highest_market_value_in_eur) {
     this.highest_market_value_in_eur = highest_market_value_in_eur;
   }
 
+  /**
+   * Gets the contract expiration date of the player.
+   *
+   * @return the contract expiration date of the player
+   */
   public LocalDate getContract_expiration_date() {
     return contract_expiration_date;
   }
 
+  /**
+   * Sets the contract expiration date of the player.
+   *
+   * @param contract_expiration_date the contract expiration date of the player
+   */
   public void setContract_expiration_date(LocalDate contract_expiration_date) {
     this.contract_expiration_date = contract_expiration_date;
   }
 
+  /**
+   * Gets the agent name of the player.
+   *
+   * @return the agent name of the player
+   */
   public String getAgent_name() {
     return agent_name;
   }
 
+  /**
+   * Sets the agent name of the player.
+   *
+   * @param agent_name the agent name of the player
+   */
   public void setAgent_name(String agent_name) {
     this.agent_name = agent_name;
   }
 
+  /**
+   * Gets the image URL of the player.
+   *
+   * @return the image URL of the player
+   */
   public String getImage_url() {
     return image_url;
   }
 
+  /**
+   * Sets the image URL of the player.
+   *
+   * @param image_url the image URL of the player
+   */
   public void setImage_url(String image_url) {
     this.image_url = image_url;
   }
 
+  /**
+   * Gets the URL of the player.
+   *
+   * @return the URL of the player
+   */
   public String getUrl() {
     return url;
   }
 
+  /**
+   * Sets the URL of the player.
+   *
+   * @param url the URL of the player
+   */
   public void setUrl(String url) {
     this.url = url;
   }
 
+  /**
+   * Gets the current club domestic competition ID.
+   *
+   * @return the current club domestic competition ID
+   */
   public String getCurrent_club_domestic_competition_id() {
     return current_club_domestic_competition_id;
   }
 
+  /**
+   * Sets the current club domestic competition ID.
+   *
+   * @param current_club_domestic_competition_id the current club domestic competition ID
+   */
   public void setCurrent_club_domestic_competition_id(String current_club_domestic_competition_id) {
     this.current_club_domestic_competition_id = current_club_domestic_competition_id;
   }
 
+  /**
+   * Gets the current club name of the player.
+   *
+   * @return the current club name of the player
+   */
   public String getCurrent_club_name() {
     return current_club_name;
   }
 
+  /**
+   * Sets the current club name of the player.
+   *
+   * @param current_club_name the current club name of the player
+   */
   public void setCurrent_club_name(String current_club_name) {
     this.current_club_name = current_club_name;
   }
 
-  public int getAge(){
+  /**
+   * Gets the age of the player.
+   *
+   * @return the age of the player
+   */
+  public int getAge() {
     return this.age;
   }
 
-  public void setAge(int age){ // forse non serve
+  /**
+   * Sets the age of the player.
+   *
+   * @param age the age of the player (possibly unused)
+   */
+  public void setAge(int age) {
     this.age = age;
   }
 
-  public int getMarket_value(){
+  /**
+   * Gets the market value of the player.
+   *
+   * @return the market value of the player
+   */
+  public int getMarket_value() {
     return this.market_value;
   }
 
-  public void setMarket_value(int value){ // forse non serve
+  /**
+   * Sets the market value of the player.
+   *
+   * @param value the market value of the player (possibly unused)
+   */
+  public void setMarket_value(int value) {
     this.market_value = value;
   }
 
-  public int getHighest_market_value(){
+  /**
+   * Gets the highest market value of the player.
+   *
+   * @return the highest market value of the player
+   */
+  public int getHighest_market_value() {
     return this.highest_market_value;
   }
 
-  public void setHighest_market_value(int value){ // forse non serve
+  /**
+   * Sets the highest market value of the player.
+   *
+   * @param value the highest market value of the player (possibly unused)
+   */
+  public void setHighest_market_value(int value) {
     this.highest_market_value = value;
   }
 
--- PostgresAPI/src/main/java/com/footbrawl/postgresapi/player/PlayerService.java
+++ PostgresAPI/src/main/java/com/footbrawl/postgresapi/player/PlayerService.java
@@ -11,15 +11,29 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Service class for managing player operations.
+ */
 @Service
 public class PlayerService {
   private final PlayerRepository playerRepository;
 
+  /**
+   * Constructor for PlayerService.
+   *
+   * @param playerRepository the player repository
+   */
   @Autowired
   public PlayerService(PlayerRepository playerRepository) {
     this.playerRepository = playerRepository;
   }
 
+  /**
+   * Retrieves a player by their ID.
+   *
+   * @param id the ID of the player
+   * @return the PlayerDTO representing the player, or null if not found
+   */
   public PlayerDTO getPlayer(int id) {
     Player player = playerRepository.findPlayerByIdCustomQuery(id).orElse(null);
     if (player == null)
@@ -27,6 +41,12 @@ public class PlayerService {
     return convertToDTO(player);
   }
 
+  /**
+   * Retrieves players by their name.
+   *
+   * @param name the name of the players
+   * @return a list of PlayerDTOs representing the players, or null if none found
+   */
   public List<PlayerDTO> getPlayerByName(String name) {
     List<Player> playerList = playerRepository.findPlayerByNameCustomQuery(name).orElse(null);
     if (playerList == null || playerList.isEmpty())
@@ -39,6 +59,13 @@ public class PlayerService {
     return playerDTOList;
   }
 
+  /**
+   * Retrieves players by their first and last name.
+   *
+   * @param firstName the first name of the players
+   * @param lastName  the last name of the players
+   * @return a list of PlayerDTOs representing the players, or null if none found
+   */
   public List<PlayerDTO> getPlayerByName2(String firstName, String lastName) {
     List<Player> playerList = playerRepository.findPlayerByName2CustomQuery(firstName, lastName).orElse(null);
     if (playerList == null || playerList.isEmpty())
@@ -51,6 +78,12 @@ public class PlayerService {
     return playerDTOList;
   }
 
+  /**
+   * Retrieves players by their club ID.
+   *
+   * @param id the ID of the club
+   * @return a list of PlayerDTOs representing the players, or null if none found
+   */
   public List<PlayerDTO> getPlayersByClubId(int id) {
     List<Player> playerList = playerRepository.findPlayersByCurrent_club_id(id).orElse(null);
     if (playerList == null || playerList.isEmpty())
@@ -63,6 +96,12 @@ public class PlayerService {
     return playerDTOList;
   }
 
+  /**
+   * Retrieves last season's players by their club ID.
+   *
+   * @param id the ID of the club
+   * @return a list of PlayerDTOs representing the players, sorted by market value in descending order, or null if none found
+   */
   public List<PlayerDTO> getLastSeasonPlayersByClubId(int id) {
     List<Player> playerList = playerRepository.findLastSeasonPlayersByCurrent_club_id(id).orElse(null);
     if (playerList == null || playerList.isEmpty())
@@ -75,6 +114,12 @@ public class PlayerService {
     return playerDTOList;
   }
 
+  /**
+   * Retrieves top market players by their competition ID.
+   *
+   * @param competitionId the ID of the competition
+   * @return a list of PlayerDTOs representing the top market players, or null if none found
+   */
   public List<PlayerDTO> getTopMarketPlayersByCompetitionId(String competitionId) {
     List<Player> topMarketPlayers = playerRepository.findTopMarketPlayersByCompetitionId(competitionId);
     if (topMarketPlayers == null || topMarketPlayers.isEmpty()) {
@@ -89,6 +134,12 @@ public class PlayerService {
     return topMarketPlayerDTOList;
   }
 
+  /**
+   * Converts a Player entity to a PlayerDTO.
+   *
+   * @param player the Player entity
+   * @return the PlayerDTO representing the player
+   */
   public PlayerDTO convertToDTO(Player player) {
     PlayerDTO playerDTO = new PlayerDTO();
     playerDTO.setPlayerId(player.getPlayer_id());
@@ -111,7 +162,12 @@ public class PlayerService {
     return playerDTO;
   }
 
-
+  /**
+   * Calculates the age of a player.
+   *
+   * @param player the Player entity
+   * @return the age of the player, or -1 if the date of birth is null
+   */
   private int calcAge(Player player) {
     LocalDate today = LocalDate.now();
     if (player.getDate_of_birth() != null) {
@@ -120,11 +176,23 @@ public class PlayerService {
     return -1;
   }
 
+  /**
+   * Calculates the market value of a player.
+   *
+   * @param player the Player entity
+   * @return the market value of the player
+   */
   private int calculateMarketValue(Player player) {
     String value = player.getMarket_value_in_eur();
     return PlayerUtils.calculateMarketValueUtils(value);
   }
 
+  /**
+   * Calculates the highest market value of a player.
+   *
+   * @param player the Player entity
+   * @return the highest market value of the player
+   */
   private int calculateHighestMarketValue(Player player) {
     String value = player.getHighest_market_value_in_eur();
     return PlayerUtils.calculateMarketValueUtils(value);
